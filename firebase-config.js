2eca36b96b3558d2b9539567088e5f5f103e97ac
adrien3344032-hue <adrien3344032@gmail.com>
2026-07-22 21:34:19 +0200

    Add files via upload

---
+++ firebase-config.js
@@ -0,0 +1,11 @@
+// Configuration Firebase à remplir.
+// Dans Firebase : Paramètres du projet > Vos applications > Web app > Configuration SDK.
+// Remplace uniquement les valeurs ci-dessous.
+window.SUIVI_OF_FIREBASE_CONFIG = {
+   apiKey: "AIzaSyDczKjNHXG2oqa4Z71qm2S8f671u3RqKKg",
+  authDomain: "elitt-1c749.firebaseapp.com",
+  projectId: "elitt-1c749",
+  storageBucket: "elitt-1c749.firebasestorage.app",
+  messagingSenderId: "6078833205",
+  appId: "1:6078833205:web:c9ee05e58691c9bf52077c"
+};
